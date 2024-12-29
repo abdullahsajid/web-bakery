1d4991bdd1ed1b538782c3706b114769662dd825
Abdullah sajid <abdullahsajid521@gmail.com>
2024-12-30 00:40:23 +0500

    refactor: update nav

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,6 +1,7 @@
 import React, { ReactNode } from 'react';
 import { SidebarProvider, SidebarTrigger } from '../_components/ui/sidebar';
 import { AppSidebar } from '../_components/app-sidebar';
+import { Avatar, AvatarFallback, AvatarImage } from '../_components/ui/avatar';
 
 export default async function DashboardLayout({
 	children,
@@ -11,7 +12,20 @@ export default async function DashboardLayout({
 		<SidebarProvider>
 			<AppSidebar />
 			<main className='w-full'>
-				<SidebarTrigger />
+				<div className='sticky top-0 z-10 flex flex-col mb-3'>
+					<header className='flex h-14 items-center justify-between gap-2 border-b bg-background px-4 lg:h-[60px]'>
+                        <SidebarTrigger />
+                        <div className='flex gap-x-4'>
+						    <Avatar>
+                                <AvatarImage
+                                    src='https://github.com/shadcn.png'
+                                    alt='@shadcn'
+                                />
+                                <AvatarFallback>CN</AvatarFallback>
+                            </Avatar>
+                        </div>
+					</header>
+				</div>
 				{children}
 			</main>
 		</SidebarProvider>
